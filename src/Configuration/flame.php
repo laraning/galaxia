d5c6888a97ea91d14eaf61b015c25733f4f5eff8
Laraning <bruno.falcao@laraning.com>
2018-08-05 23:45:59 +0000

    Apply fixes from StyleCI

--- src/Configuration/flame.php
+++ src/Configuration/flame.php
@@ -3,8 +3,8 @@
 return [
     'galaxia' => [
         'namespace' => 'App\Galaxia\Features',
-        'path'      => base_path('app' .
-                       DIRECTORY_SEPARATOR . 'Galaxia' .
+        'path'      => base_path('app'.
+                       DIRECTORY_SEPARATOR.'Galaxia'.
                        DIRECTORY_SEPARATOR.'Features'),
     ],
 ];
